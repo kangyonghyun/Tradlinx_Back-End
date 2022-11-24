e173089ba273fd42f6922493a328d92cd11b0789
kanghyonghyun <hmoon826@gmail.com>
2022-11-24 14:08:46 +0900

    [feat] request 회원가입 form

---
+++ src/main/java/com/tradlinx/account/form/NewAccountForm.java
@@ -0,0 +1,12 @@
+package com.tradlinx.account.form;
+
+import lombok.Data;
+
+@Data
+public class NewAccountForm {
+
+    private String userid;
+    private String pw;
+    private String username;
+
+}
---
+++ src/main/java/com/tradlinx/account/form/SignUpForm.java
@@ -0,0 +1,12 @@
+package com.tradlinx.account.form;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String userid;
+    private String pw;
+    private String username;
+
+}
