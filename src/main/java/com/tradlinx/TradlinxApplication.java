b76b5b5b0b4ea1ee858bdff78b9edc01da92bdd2
kanghyonghyun <hmoon826@gmail.com>
2022-11-24 12:48:44 +0900

    init commit

---
+++ src/main/java/com/tradlinx/TradlinxApplication.java
@@ -0,0 +1,13 @@
+package com.tradlinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradlinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradlinxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradlinx/TradlinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradlinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradlinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
