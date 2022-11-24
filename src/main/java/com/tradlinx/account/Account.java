ebaa8528efab88e3173483c048e3295a3bf6e27e
kanghyonghyun <hmoon826@gmail.com>
2022-11-24 14:05:49 +0900

    [chore] 회원 도메인 수정

--- src/main/java/com/tradlinx/account/Account.java
+++ src/main/java/com/tradlinx/account/Account.java
@@ -3,16 +3,13 @@ package com.tradlinx.account;
 import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
-@EqualsAndHashCode(of = "userid")
-@AllArgsConstructor @NoArgsConstructor
 public class Account {
 
-    @Id @GeneratedValue
+    @Id
     String userid;
     String pw;
     String username;
