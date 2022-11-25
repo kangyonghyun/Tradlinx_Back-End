246a5a5a6cc17411216847515c89c8099fbfa36c
kanghyonghyun <hmoon826@gmail.com>
2022-11-25 20:02:45 +0900

    [refactor] security 설정

--- src/main/java/com/tradlinx/config/SecurityConfig.java
+++ src/main/java/com/tradlinx/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,13 +19,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests()
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeHttpRequests()
                 .mvcMatchers(HttpMethod.POST, "/signup").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/signin").permitAll()
-                .anyRequest().authenticated();
-        http.csrf().disable();
-                http.apply(new JwtSecurityConfig(tokenProvider));
+                .anyRequest().authenticated()
+            .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
         return http.build();
     }
