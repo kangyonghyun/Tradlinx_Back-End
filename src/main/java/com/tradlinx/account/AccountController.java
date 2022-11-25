073c067153c8f7a35e1e3966d1690e4e1999010b
kanghyonghyun <hmoon826@gmail.com>
2022-11-25 17:44:22 +0900

    [refactor] 회원가입 응답 수정

--- src/main/java/com/tradlinx/account/AccountController.java
+++ src/main/java/com/tradlinx/account/AccountController.java
@@ -4,6 +4,7 @@ import com.tradlinx.account.form.LoginForm;
 import com.tradlinx.account.form.NewSignUpDto;
 import com.tradlinx.account.form.SignUpDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +16,9 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/signup")
-    public NewSignUpDto SignUp(@RequestBody SignUpDto signUpDto) {
-        return accountService.processNewAccount(signUpDto);
+    public ResponseEntity<String> SignUp(@RequestBody SignUpDto signUpDto) {
+        String userid = accountService.processNewAccount(signUpDto);
+        return new ResponseEntity<>(userid + "님 가입 완료했습니다.", HttpStatus.OK);
     }
 
-    @PostMapping("/signin")
-    public AccessToken login(@RequestBody LoginForm loginForm) {
-        Account account = accountService.processLogin(loginForm);
-        if (account == null) {
-            return new AccessToken("Authorization : No!!");
-        }
-        return new AccessToken("Authorization: Bearer JwtToken");
-    }
-
-//    @GetMapping("/profile")
-//    public ProfileForm viewProfile(AccessToken accessToken) {
-//
-//    }
 }
