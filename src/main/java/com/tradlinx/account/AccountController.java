2296bc2461be897870cdc3ada62d3999bccb0ac9
kanghyonghyun <hmoon826@gmail.com>
2022-11-25 18:14:10 +0900

    [feat] 로그인 기능 추가 (JWT 토큰 발행)

--- src/main/java/com/tradlinx/account/AccountController.java
+++ src/main/java/com/tradlinx/account/AccountController.java
@@ -1,10 +1,15 @@
 package com.tradlinx.account;
 
-import com.tradlinx.account.form.LoginForm;
-import com.tradlinx.account.form.NewSignUpDto;
+import com.tradlinx.account.form.LoginDto;
 import com.tradlinx.account.form.SignUpDto;
+import com.tradlinx.jwt.JwtFilter;
+import com.tradlinx.jwt.JwtToken;
+import com.tradlinx.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountService accountService;
+    private final JwtTokenProvider tokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<String> SignUp(@RequestBody SignUpDto signUpDto) {
         String userid = accountService.processNewAccount(signUpDto);
         return new ResponseEntity<>(userid + "님 가입 완료했습니다.", HttpStatus.OK);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtToken> signin(@RequestBody LoginDto loginDto) {
+        Authentication authentication = accountService.processLogin(loginDto);
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new JwtToken(jwt), httpHeaders, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/tradlinx/account/AccountService.java
+++ src/main/java/com/tradlinx/account/AccountService.java
@@ -1,26 +1,39 @@
 package com.tradlinx.account;
 
-import com.tradlinx.account.form.LoginForm;
-import com.tradlinx.account.form.NewSignUpDto;
+import com.tradlinx.account.form.LoginDto;
 import com.tradlinx.account.form.SignUpDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
-    private final ModelMapper modelMapper;
     private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public NewSignUpDto processNewAccount(SignUpDto signUpDto) {
+    public String processNewAccount(SignUpDto signUpDto) {
+        signUpDto.setPw(passwordEncoder.encode(signUpDto.getPw()));
         Account account = accountRepository.save(modelMapper.map(signUpDto, Account.class));
-        return modelMapper.map(account, NewSignUpDto.class);
+        return account.getUserid();
     }
 
-    public Account processLogin(LoginForm loginForm) {
-        return accountRepository.findByUseridAndPw(loginForm.getUserid(), loginForm.getPw());
+    public Authentication processLogin(LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUserid(), loginDto.getPw());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
     }
 
 }
