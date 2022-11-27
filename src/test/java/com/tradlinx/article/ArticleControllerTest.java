8f1b571a8f8fac7ea96d27106fabde56ece564ab
kanghyonghyun <hmoon826@gmail.com>
2022-11-27 18:03:28 +0900

    [test] 글 수정 기능 테스트

--- src/test/java/com/tradlinx/article/ArticleControllerTest.java
+++ src/test/java/com/tradlinx/article/ArticleControllerTest.java
@@ -2,8 +2,10 @@ package com.tradlinx.article;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tradlinx.account.Account;
 import com.tradlinx.account.AccountRepository;
 import com.tradlinx.account.AccountService;
+import com.tradlinx.account.form.ArticleUpdateDto;
 import com.tradlinx.account.form.SignUpDto;
 import com.tradlinx.article.form.ArticleDto;
 import org.assertj.core.api.Assertions;
@@ -20,15 +22,19 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class ArticleControllerTest {
 
     @Autowired
@@ -41,6 +47,8 @@ class ArticleControllerTest {
     AccountService accountService;
     @Autowired
     ArticleRepository articleRepository;
+    @Autowired
+    ArticleService articleService;
 
     SignUpDto signUpDto;
     @BeforeEach
@@ -55,11 +63,12 @@ class ArticleControllerTest {
     @AfterEach
     void afterEach() {
         accountRepository.deleteAll();
+        articleRepository.deleteAll();
     }
 
     @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
-    @DisplayName("글 작성 - 성공")
+    @DisplayName("글 작성 - 포인트 증가 +3")
     void article_write_success() throws Exception {
         ArticleDto articleDto = new ArticleDto();
         articleDto.setArticleTitle("articleTitle");
@@ -74,8 +83,38 @@ class ArticleControllerTest {
                 .andExpect(content().string("articleId"));
 
         Article article = articleRepository.findById("articleId").orElseThrow();
-        Assertions.assertThat(article.getArticleTitle()).isEqualTo("articleTitle");
-        Assertions.assertThat(article.getArticleContents()).isEqualTo("articleContents");
+        assertThat(article.getArticleTitle()).isEqualTo("articleTitle");
+        assertThat(article.getArticleContents()).isEqualTo("articleContents");
+
+        Account account = accountRepository.findById("userid").orElseThrow();
+        assertThat(account.getPoints()).isEqualTo(3);
+    }
+
+    @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    @DisplayName("글 수정")
+    void article_update_success() throws Exception {
+        ArticleDto articleDto = new ArticleDto();
+        articleDto.setArticleTitle("articleTitle");
+        articleDto.setArticleContents("articleContents");
+        articleService.writeArticle(articleDto);
+
+        ArticleUpdateDto articleUpdateDto = new ArticleUpdateDto();
+        articleUpdateDto.setArticleId("articleId");
+        articleUpdateDto.setArticleTitle("updateArticleTitle");
+        articleUpdateDto.setArticleContents("updateArticleContents");
+
+        mockMvc.perform(put("/article")
+                        .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer "))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(articleUpdateDto)))
+                .andExpect(status().isOk())
+                .andExpect(authenticated())
+                .andExpect(content().string("articleId"));
+
+        Article article = articleRepository.findById("articleId").orElseThrow();
+        assertThat(article.getArticleTitle()).isEqualTo("updateArticleTitle");
+        assertThat(article.getArticleContents()).isEqualTo("updateArticleContents");
     }
 
 }
