16e4dad89fa6a9947b524044e007b2270cea5ed2
kanghyonghyun <hmoon826@gmail.com>
2022-11-27 14:44:51 +0900

    [test] 권한 체크 테스트 추가

--- src/test/java/com/tradlinx/account/AccountControllerTest.java
+++ src/test/java/com/tradlinx/account/AccountControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tradlinx.account.form.LoginDto;
 import com.tradlinx.account.form.SignUpDto;
 import org.hamcrest.core.StringStartsWith;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -46,18 +49,26 @@ class AccountControllerTest {
         accountService.processNewAccount(signUpDto);
     }
 
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("회원 가입 - 성공")
     void signUp_correct_input() throws Exception {
         mockMvc.perform(post("/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(signUpDto)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(unauthenticated());
 
         Account newAccount = accountRepository.findById("userid").orElseThrow();
         assertThat(newAccount.getUserid()).isEqualTo("userid");
         assertThat(newAccount.getPw()).isNotEqualTo("passw0rd");
     }
+
+    @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("로그인 - 성공")
     void login_success() throws Exception {
@@ -68,8 +79,9 @@ class AccountControllerTest {
         mockMvc.perform(post("/signin")
                         .content(objectMapper.writeValueAsString(loginDto))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
                 .andExpect(header().string(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
+                .andExpect(status().isOk())
+                .andExpect(authenticated().withUsername("userid"))
                 .andDo(print());
     }
 
@@ -81,7 +93,8 @@ class AccountControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("userid", is("userid")))
-                .andExpect(jsonPath("username", is("username")));
+                .andExpect(jsonPath("username", is("username")))
+                .andExpect(authenticated().withUsername("userid"));
     }
 
     @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
@@ -91,7 +104,8 @@ class AccountControllerTest {
         mockMvc.perform(get("/points")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("points", is(0)));
+                .andExpect(jsonPath("points", is(0)))
+                .andExpect(authenticated().withUsername("userid"));
     }
 
 }
