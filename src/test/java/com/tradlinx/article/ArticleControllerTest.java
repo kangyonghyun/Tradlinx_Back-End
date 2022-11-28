0451b4655903f96773848bc94acf438f1fde9c0f
kanghyonghyun <hmoon826@gmail.com>
2022-11-28 18:10:26 +0900

    [test] 중복 수정

--- src/test/java/com/tradlinx/article/ArticleControllerTest.java
+++ src/test/java/com/tradlinx/article/ArticleControllerTest.java
@@ -51,6 +51,8 @@ class ArticleControllerTest {
     ArticleService articleService;
 
     SignUpDto signUpDto;
+    ArticleDto articleDto;
+
     @BeforeEach
     void beforeEach() {
         signUpDto = new SignUpDto();
@@ -58,29 +60,29 @@ class ArticleControllerTest {
         signUpDto.setPw("passw0rd");
         signUpDto.setUsername("username");
         accountService.processNewAccount(signUpDto);
+
+        articleDto = new ArticleDto();
+        articleDto.setArticleTitle("articleTitle");
+        articleDto.setArticleContents("articleContents");
     }
 
     @AfterEach
     void afterEach() {
-        accountRepository.deleteAll();
-        articleRepository.deleteAll();
+//        accountRepository.deleteAll();
+//        articleRepository.deleteAll();
     }
 
     @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("글 작성 - 포인트 증가 +3")
     void article_write_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
-
         mockMvc.perform(post("/article")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer "))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(articleDto)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(content().string("articleId"));
+                .andExpect(jsonPath("articleId", is("articleId")));
 
         Article article = articleRepository.findById("articleId").orElseThrow();
         assertThat(article.getArticleTitle()).isEqualTo("articleTitle");
@@ -94,9 +96,6 @@ class ArticleControllerTest {
     @Test
     @DisplayName("글 수정")
     void article_update_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
         articleService.writeArticle(articleDto);
 
         ArticleUpdateDto articleUpdateDto = new ArticleUpdateDto();
@@ -110,7 +109,7 @@ class ArticleControllerTest {
                         .content(objectMapper.writeValueAsString(articleUpdateDto)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(content().string("articleId"));
+                .andExpect(jsonPath("articleId", is("articleId")));
 
         Article article = articleRepository.findById("articleId").orElseThrow();
         assertThat(article.getArticleTitle()).isEqualTo("updateArticleTitle");
@@ -121,9 +120,6 @@ class ArticleControllerTest {
     @Test
     @DisplayName("글 삭제 - 포인트 감소 -3")
     void article_delete_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
         articleService.writeArticle(articleDto);
 
         Account account = accountRepository.findById("userid").orElseThrow();
@@ -133,7 +129,7 @@ class ArticleControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(content().string("1"));
+                .andExpect(jsonPath("count", is(1)));
 
         assertThat(articleRepository.findById("articleId")).isEmpty();
         assertThat(account.getPoints()).isEqualTo(0);
@@ -143,9 +139,6 @@ class ArticleControllerTest {
     @Test
     @DisplayName("글 조회 - commentId 응답")
     void article_get_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
         articleService.writeArticle(articleDto);
 
         mockMvc.perform(get("/article/articleId")
@@ -160,9 +153,6 @@ class ArticleControllerTest {
     @Test
     @DisplayName("댓글 작성 - commentId 응답, 댓글 작성자 points +2, 원글 작성자 points +1")
     void comment_write_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
         articleService.writeArticle(articleDto);
 
         anotherAccountAndLogin();
@@ -177,7 +167,7 @@ class ArticleControllerTest {
                         .content(objectMapper.writeValueAsString(commentDto)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(content().string("commentId"));
+                .andExpect(jsonPath("commentId", is("commentId")));
 
         Article article = articleRepository.findById("articleId").orElseThrow();
         assertThat(article.getAccount().getPoints()).isEqualTo(4);
@@ -186,25 +176,10 @@ class ArticleControllerTest {
         assertThat(account.getPoints()).isEqualTo(2);
     }
 
-    private void anotherAccountAndLogin() {
-        SignUpDto signUpDto1 = new SignUpDto();
-        signUpDto1.setUserid("userid2");
-        signUpDto1.setPw("passw0rd");
-        signUpDto1.setUsername("username");
-        accountService.processNewAccount(signUpDto1);
-        LoginDto loginDto = new LoginDto();
-        loginDto.setUserid("userid2");
-        loginDto.setPw("passw0rd");
-        accountService.processLogin(loginDto);
-    }
-
     @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("댓글 삭제 - commentId 응답, 댓글 작성자 points -2, 원글 작성자 points -1")
     void comment_delete_success() throws Exception {
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setArticleTitle("articleTitle");
-        articleDto.setArticleContents("articleContents");
         articleService.writeArticle(articleDto);
 
         anotherAccountAndLogin();
@@ -218,13 +193,25 @@ class ArticleControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(content().string("commentId"));
+                .andExpect(jsonPath("commentId", is("commentId")));
 
         Article article = articleRepository.findById("articleId").orElseThrow();
         assertThat(article.getAccount().getPoints()).isEqualTo(3);
 
         Account account = accountRepository.findById("userid2").orElseThrow();
         assertThat(account.getPoints()).isEqualTo(0);
+    }
+
+    private void anotherAccountAndLogin() {
+        SignUpDto signUpDto1 = new SignUpDto();
+        signUpDto1.setUserid("userid2");
+        signUpDto1.setPw("passw0rd");
+        signUpDto1.setUsername("username");
+        accountService.processNewAccount(signUpDto1);
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUserid("userid2");
+        loginDto.setPw("passw0rd");
+        accountService.processLogin(loginDto);
     }
 
 }
