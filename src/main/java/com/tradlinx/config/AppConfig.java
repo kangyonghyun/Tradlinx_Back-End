5dec160a4b06f6e31ce0fdbaba38f96c73b305aa
kanghyonghyun <hmoon826@gmail.com>
2022-11-30 17:11:55 +0900

    [refactor] modelmapper 정확하게 이름이 매치될 경우만 매핑 설정

--- src/main/java/com/tradlinx/config/AppConfig.java
+++ src/main/java/com/tradlinx/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.tradlinx.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -14,7 +15,9 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        return new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
     @Bean
