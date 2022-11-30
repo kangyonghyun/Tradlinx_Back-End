8ea640d5e3916dbef71f5be6359ea5c24e83b26e
kanghyonghyun <hmoon826@gmail.com>
2022-11-30 17:15:34 +0900

    [test] 회원가입 시, 실패 검증 테스트 추가

--- src/test/java/com/tradlinx/api/account/AccountControllerTest.java
+++ src/test/java/com/tradlinx/api/account/AccountControllerTest.java
@@ -1,9 +1,6 @@
 package com.tradlinx.api.account;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tradlinx.api.account.Account;
-import com.tradlinx.api.account.AccountRepository;
-import com.tradlinx.api.account.AccountService;
 import com.tradlinx.api.account.form.LoginDto;
 import com.tradlinx.api.account.form.SignUpDto;
 import org.hamcrest.core.StringStartsWith;
@@ -43,10 +40,11 @@ class AccountControllerTest {
     AccountService accountService;
 
     SignUpDto signUpDto;
+
     @BeforeEach
     void beforeEach() {
         signUpDto = new SignUpDto();
-        signUpDto.setUserid("userid");
+        signUpDto.setUserId("userid1");
         signUpDto.setPw("passw0rd");
         signUpDto.setUsername("username");
         accountService.processNewAccount(signUpDto);
@@ -60,23 +58,44 @@ class AccountControllerTest {
     @Test
     @DisplayName("회원 가입 - 성공")
     void signUp_correct_input() throws Exception {
+        signUpDto = new SignUpDto();
+        signUpDto.setUserId("userid");
+        signUpDto.setPw("passw0rd");
+        signUpDto.setUsername("username");
+
         mockMvc.perform(post("/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(signUpDto)))
                 .andExpect(status().isOk())
                 .andExpect(unauthenticated());
 
-        Account newAccount = accountRepository.findById("userid").orElseThrow();
-        assertThat(newAccount.getUserid()).isEqualTo("userid");
+        Account newAccount = accountRepository.findByUserId("userid").orElseThrow();
+        assertThat(newAccount.getUserId()).isEqualTo("userid");
         assertThat(newAccount.getPw()).isNotEqualTo("passw0rd");
     }
 
-    @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    @DisplayName("회원 가입 - 실패 (userId 잘못입력)")
+    void signUp_incorrect_input() throws Exception {
+        signUpDto = new SignUpDto();
+        signUpDto.setUserId("Tuserid");
+        signUpDto.setPw("passw0rd");
+        signUpDto.setUsername("username");
+
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpDto)))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("code", is("BAD")))
+                .andExpect(jsonPath("message", is("ID 를 잘못 입력했습니다. 다시 입력해주세요")));
+    }
+
+    @WithUserDetails(value = "userid1", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("로그인 - 성공")
     void login_success() throws Exception {
         LoginDto loginDto = new LoginDto();
-        loginDto.setUserid("userid");
+        loginDto.setUserId("userid1");
         loginDto.setPw("passw0rd");
 
         mockMvc.perform(post("/signin")
@@ -84,31 +103,32 @@ class AccountControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(header().string(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
-                .andExpect(authenticated().withUsername("userid"))
+                .andExpect(authenticated().withUsername("userid1"))
                 .andDo(print());
     }
 
-    @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @WithUserDetails(value = "userid1", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("프로필 조회")
     void get_profile() throws Exception {
         mockMvc.perform(get("/profile")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("userid", is("userid")))
+                .andExpect(jsonPath("userId", is("userid1")))
                 .andExpect(jsonPath("username", is("username")))
-                .andExpect(authenticated().withUsername("userid"));
+                .andExpect(authenticated().withUsername("userid1"));
     }
 
-    @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @WithUserDetails(value = "userid1", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @Test
     @DisplayName("포인트 조회")
     void get_points() throws Exception {
+
         mockMvc.perform(get("/points")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("points", is(0)))
-                .andExpect(authenticated().withUsername("userid"));
+                .andExpect(authenticated().withUsername("userid1"));
     }
 
 }
