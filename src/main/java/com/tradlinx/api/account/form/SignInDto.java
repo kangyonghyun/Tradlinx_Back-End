4aeeac8b6d1fb4d7a77287df6eac0df44eb4592c
kanghyonghyun <hmoon826@gmail.com>
2022-12-02 12:29:00 +0900

    [add] 로그인 검증 로직 추가

--- src/main/java/com/tradlinx/api/account/form/SignInDto.java
+++ src/main/java/com/tradlinx/api/account/form/SignInDto.java
@@ -2,8 +2,12 @@ package com.tradlinx.api.account.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
-public class LoginDto {
+public class SignInDto {
+    @NotBlank
     private String userId;
+    @NotBlank
     private String pw;
 }
