37c96fc6ee62e0a8d555fe0442317d9afc371fe4
kanghyonghyun <hmoon826@gmail.com>
2022-12-07 18:32:57 +0900

    [refactor] 글,댓글 작성 요청시 검증 추가

--- src/main/java/com/tradlinx/api/article/dto/ArticleUpdateRequest.java
+++ src/main/java/com/tradlinx/api/article/dto/ArticleUpdateRequest.java
@@ -2,9 +2,16 @@ package com.tradlinx.api.article.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class ArticleUpdateRequest {
+
     private Long articleId;
+
+    @NotBlank
     private String articleTitle;
+
     private String articleContents;
+
 }
--- src/main/java/com/tradlinx/api/article/dto/CommentWriteRequest.java
+++ src/main/java/com/tradlinx/api/article/dto/CommentWriteRequest.java
@@ -1,9 +1,15 @@
-package com.tradlinx.api.article.form;
+package com.tradlinx.api.article.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class CommentWriteRequest {
+
     private Long articleId;
+
+    @NotBlank
     private String commentContents;
+
 }
