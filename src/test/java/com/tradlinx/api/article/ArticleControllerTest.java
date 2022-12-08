0eb7cf85d6ee475582a6d4d2afc9c3eaacbdae23
kanghyonghyun <hmoon826@gmail.com>
2022-12-08 19:22:45 +0900

    [test] 글,댓글 작성,수정,삭제 테스트

--- src/test/java/com/tradlinx/api/article/ArticleControllerTest.java
+++ src/test/java/com/tradlinx/api/article/ArticleControllerTest.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tradlinx.api.account.Account;
 import com.tradlinx.api.account.AccountRepository;
 import com.tradlinx.api.account.AccountService;
-import com.tradlinx.api.account.form.LoginDto;
-import com.tradlinx.api.article.form.ArticleUpdateDto;
-import com.tradlinx.api.account.form.SignUpDto;
-import com.tradlinx.api.article.form.ArticleDto;
-import com.tradlinx.api.article.form.CommentDto;
+import com.tradlinx.api.account.dto.AccountLoginRequest;
+import com.tradlinx.api.article.dto.ArticleUpdateRequest;
+import com.tradlinx.api.account.dto.AccountSaveRequest;
+import com.tradlinx.api.article.dto.ArticleWriteRequest;
+import com.tradlinx.api.article.dto.CommentWriteRequest;
+import com.tradlinx.api.comment.CommentRepository;
 import org.hamcrest.core.StringStartsWith;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -49,27 +50,35 @@ class ArticleControllerTest {
     ArticleRepository articleRepository;
     @Autowired
     ArticleService articleService;
+    @Autowired
+    CommentRepository commentRepository;
 
-    SignUpDto signUpDto;
-    ArticleDto articleDto;
+    AccountSaveRequest accountSaveRequest;
+    ArticleWriteRequest articleDto;
 
     @BeforeEach
     void beforeEach() {
-        signUpDto = new SignUpDto();
-        signUpDto.setUserId("userid");
-        signUpDto.setPw("passw0rd");
-        signUpDto.setUsername("username");
-        accountService.processNewAccount(signUpDto);
+        accountSaveRequest = new AccountSaveRequest();
+        accountSaveRequest.setUserId("userid");
+        accountSaveRequest.setPw("passw0rd");
+        accountSaveRequest.setUsername("username");
+        accountService.processNewAccount(accountSaveRequest);
 
-        articleDto = new ArticleDto();
+        accountSaveRequest = new AccountSaveRequest();
+        accountSaveRequest.setUserId("userid2");
+        accountSaveRequest.setPw("passw0rd");
+        accountSaveRequest.setUsername("username");
+        accountService.processNewAccount(accountSaveRequest);
+
+        articleDto = new ArticleWriteRequest();
         articleDto.setArticleTitle("articleTitle");
         articleDto.setArticleContents("articleContents");
     }
 
     @AfterEach
     void afterEach() {
-//        accountRepository.deleteAll();
-//        articleRepository.deleteAll();
+        articleRepository.deleteAll();
+        accountRepository.deleteAll();
     }
 
     @WithUserDetails(value = "userid", setupBefore = TestExecutionEvent.TEST_EXECUTION)
@@ -82,9 +91,9 @@ class ArticleControllerTest {
                         .content(objectMapper.writeValueAsString(articleDto)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(jsonPath("articleId", is("articleId")));
+                .andExpect(jsonPath("articleId", is(3)));
 
-        Article article = articleRepository.findById("articleId").orElseThrow();
+        Article article = articleRepository.findById(3L).orElseThrow();
         assertThat(article.getArticleTitle()).isEqualTo("articleTitle");
         assertThat(article.getArticleContents()).isEqualTo("articleContents");
 
@@ -98,20 +107,20 @@ class ArticleControllerTest {
     void article_update_success() throws Exception {
         articleService.writeArticle(articleDto);
 
-        ArticleUpdateDto articleUpdateDto = new ArticleUpdateDto();
-        articleUpdateDto.setArticleId("articleId");
-        articleUpdateDto.setArticleTitle("updateArticleTitle");
-        articleUpdateDto.setArticleContents("updateArticleContents");
+        ArticleUpdateRequest articleUpdateRequest = new ArticleUpdateRequest();
+        articleUpdateRequest.setArticleId(2L);
+        articleUpdateRequest.setArticleTitle("updateArticleTitle");
+        articleUpdateRequest.setArticleContents("updateArticleContents");
 
         mockMvc.perform(put("/article")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer "))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(articleUpdateDto)))
+                        .content(objectMapper.writeValueAsString(articleUpdateRequest)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(jsonPath("articleId", is("articleId")));
+                .andExpect(jsonPath("articleId", is(2)));
 
-        Article article = articleRepository.findById("articleId").orElseThrow();
+        Article article = articleRepository.findById(2L).orElseThrow();
         assertThat(article.getArticleTitle()).isEqualTo("updateArticleTitle");
         assertThat(article.getArticleContents()).isEqualTo("updateArticleContents");
     }
@@ -125,13 +134,13 @@ class ArticleControllerTest {
         Account account = accountRepository.findByUserId("userid").orElseThrow();
         assertThat(account.getPoints()).isEqualTo(3);
 
-        mockMvc.perform(delete("/article/articleId")
+        mockMvc.perform(delete("/article/2")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
                 .andExpect(jsonPath("count", is(1)));
 
-        assertThat(articleRepository.findById("articleId")).isEmpty();
+        assertThat(articleRepository.findById(2L)).isEmpty();
         assertThat(account.getPoints()).isEqualTo(0);
     }
 
@@ -141,11 +150,11 @@ class ArticleControllerTest {
     void article_get_success() throws Exception {
         articleService.writeArticle(articleDto);
 
-        mockMvc.perform(get("/article/articleId")
+        mockMvc.perform(get("/article/2")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(jsonPath("articleId", is("articleId")))
+                .andExpect(jsonPath("articleId", is(2)))
                 .andExpect(jsonPath("commentsId", is(new ArrayList())));
     }
 
@@ -157,19 +166,19 @@ class ArticleControllerTest {
 
         anotherAccountAndLogin();
 
-        CommentDto commentDto = new CommentDto();
-        commentDto.setArticleId("articleId");
-        commentDto.setCommentContents("commentsContents");
+        CommentWriteRequest commentWriteRequest = new CommentWriteRequest();
+        commentWriteRequest.setArticleId(2L);
+        commentWriteRequest.setCommentContents("commentsContents");
 
         mockMvc.perform(post("/comments")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer "))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(commentDto)))
+                        .content(objectMapper.writeValueAsString(commentWriteRequest)))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(jsonPath("commentId", is("commentId")));
+                .andExpect(jsonPath("commentId", is(4)));
 
-        Article article = articleRepository.findById("articleId").orElseThrow();
+        Article article = articleRepository.findById(2L).orElseThrow();
         assertThat(article.getAccount().getPoints()).isEqualTo(4);
 
         Account account = accountRepository.findByUserId("userid2").orElseThrow();
@@ -184,18 +193,18 @@ class ArticleControllerTest {
 
         anotherAccountAndLogin();
 
-        CommentDto commentDto = new CommentDto();
-        commentDto.setArticleId("articleId");
-        commentDto.setCommentContents("commentsContents");
-        articleService.writeComment(commentDto);
+        CommentWriteRequest commentWriteRequest = new CommentWriteRequest();
+        commentWriteRequest.setArticleId(2L);
+        commentWriteRequest.setCommentContents("commentsContents");
+        articleService.writeComment(commentWriteRequest);
 
-        mockMvc.perform(delete("/comments/commentId")
+        mockMvc.perform(delete("/comments/4")
                         .header(HttpHeaders.AUTHORIZATION, new StringStartsWith("Bearer ")))
                 .andExpect(status().isOk())
                 .andExpect(authenticated())
-                .andExpect(jsonPath("commentId", is("commentId")));
+                .andExpect(jsonPath("commentId", is(4)));
 
-        Article article = articleRepository.findById("articleId").orElseThrow();
+        Article article = articleRepository.findById(2L).orElseThrow();
         assertThat(article.getAccount().getPoints()).isEqualTo(3);
 
         Account account = accountRepository.findByUserId("userid2").orElseThrow();
@@ -203,15 +212,10 @@ class ArticleControllerTest {
     }
 
     private void anotherAccountAndLogin() {
-        SignUpDto signUpDto1 = new SignUpDto();
-        signUpDto1.setUserId("userid2");
-        signUpDto1.setPw("passw0rd");
-        signUpDto1.setUsername("username");
-        accountService.processNewAccount(signUpDto1);
-        LoginDto loginDto = new LoginDto();
-        loginDto.setUserId("userid2");
-        loginDto.setPw("passw0rd");
-        accountService.processLogin(loginDto);
+        AccountLoginRequest accountLoginRequest = new AccountLoginRequest();
+        accountLoginRequest.setUserId("userid2");
+        accountLoginRequest.setPw("passw0rd");
+        accountService.processLogin(accountLoginRequest);
     }
 
 }
